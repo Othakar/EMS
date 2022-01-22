dcbed891fc69ab2b4524f28ff836fe0121da5244
Thibaut jallois <thibaut.jallois@outlook.fr>
2022-01-22 15:17:42 +0100

    Ajout code erreurs

---
+++ src/EMS.Domain.Shared/EMSErrorCodes.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EMS
+{
+    public static class EMSErrorCodes
+    {
+        #region Citizen
+        public const string CitizenAlreadyExist = "EMS:1001";
+        public const string CitizenSizeMustBeGreaterThanZero = "EMS:1002";
+        public const string CitizenWeightMustBeGreaterThanZero = "EMS:1002";
+        public const string CitizenDoNotExist = "EMS:1003";
+        #endregion
+    }
+}
