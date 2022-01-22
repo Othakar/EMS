5ec6ae07f7bec91843bf00dfb56cbd3b8c072fe7
Thibaut jallois <thibaut.jallois@outlook.fr>
2022-01-22 16:30:23 +0100

    Ajout action

---
+++ src/EMS.Domain/Actions/Action.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities;
+
+namespace EMS.Actions
+{
+    public class Action : Entity<int>
+    {
+        #region Constructors
+        private Action() { }
+
+        internal Action(string name, int price, string description, ActionType type)
+        {
+            Name = name;
+            this.SetPrice(price);
+            Description = description;
+            Type = type;
+        }
+        #endregion
+
+        #region Propeties
+        [Required]
+        public string Name { get; private set; }
+
+        [Required]
+        public int Price { get; private set; }
+
+        [Required]
+        public string Description { get; private set; }
+
+        [Required]
+        public ActionType Type { get; private set; }
+
+        #endregion
+
+        #region Setters
+        private void SetPrice(int price)
+        {
+            if(price <= 0)
+            {
+                throw new BusinessException(EMSErrorCodes.ActionPriceMustBeGreaterThanZero);
+            }
+            this.Price = price;
+        } 
+        #endregion
+    }
+}
---
+++ src/EMS.Domain/Actions/ActionDomainService.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Services;
+using Volo.Abp.Uow;
+
+namespace EMS.Actions
+{
+    public class ActionDomainService : DomainService
+    {
+        private readonly IRepository<Action, int> _actionRepository;
+        private readonly UnitOfWorkManager _unitOfWorkManager;
+
+        public ActionDomainService(IRepository<Action, int> actionRepository, UnitOfWorkManager unitOfWorkManager)
+        {
+            _actionRepository = actionRepository;
+            _unitOfWorkManager = unitOfWorkManager;
+        }
+
+        [UnitOfWork]
+        public virtual async Task<Action> Create(string name, int price, string description, ActionType type)
+        {
+           var existingAction = await _actionRepository.FirstOrDefaultAsync(a => a.Name == name);
+            if (existingAction == null)
+            {
+                throw new BusinessException(EMSErrorCodes.ActionAlreadyExist);
+            }
+            var newAction = new Action(name,price,description,type);
+            return await _actionRepository.InsertAsync(newAction);
+        }
+    }
+}
