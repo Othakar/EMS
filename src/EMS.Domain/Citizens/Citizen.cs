e144f01ad95337838b04a8ccbe2ac48af831f66c
Thibaut jallois <thibaut.jallois@outlook.fr>
2022-01-22 15:17:22 +0100

    Ajout de l'entité personnage et domain service

---
+++ src/EMS.Domain/Citizens/Citizen.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities;
+
+namespace EMS.Citizens
+{
+    public class Citizen : Entity<int>, ISoftDelete
+
+    {
+        private Citizen()
+        {
+
+        }
+
+        internal Citizen(string name,string surname,decimal size,decimal weight,DateOnly birthDate, string jobName,  int phoneNumber, BloodType bloodType, bool isDoingDrug = false, bool haveInsurance = false)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.SetSize(size);
+            this.SetWeigth(weight);
+            this.BirthDate = birthDate;
+            this.JobName = jobName;
+            this.PhoneNumber = phoneNumber;
+            this.IsDoingDrug = isDoingDrug;
+            this.HaveInsurance = haveInsurance;
+            this.BloodType = bloodType;
+        }
+
+        #region properties
+        [Required]
+        public string Name { get; private set; }
+
+        [Required]
+        public string Surname { get; private set; }
+
+        [Required]
+        public decimal Weight { get; private set; }
+
+        [Required]
+        public decimal Size { get; private set; }
+
+        [Required]
+        public DateOnly BirthDate { get; private set; }
+
+        [Required]
+        public bool IsDoingDrug { get; private set; }
+
+        [Required]
+        public bool HaveInsurance { get; private set; }
+
+        [Required]
+        public int PhoneNumber { get; private set; }
+
+        [Required]
+        public string JobName { get; private set; }// note ça pourrait etre une enum
+
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        [Required]
+        public BloodType BloodType { get; private set; }
+        #endregion
+
+        #region setter
+        private void SetSize(decimal size)
+        {
+            if(size <= 0)
+            {
+                throw new BusinessException(EMSErrorCodes.CitizenSizeMustBeGreaterThanZero);
+            }
+            this.Size = size;
+        }
+
+        private void SetWeigth(decimal weigth)
+        {
+            if (weigth <= 0)
+            {
+                throw new BusinessException(EMSErrorCodes.CitizenWeightMustBeGreaterThanZero);
+            }
+            this.Weight = weigth;
+        }
+
+        #endregion
+    }
+}
---
+++ src/EMS.Domain/Citizens/CitizenDomainService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Services;
+using Volo.Abp.Uow;
+
+namespace EMS.Citizens
+{
+    public class CitizenDomainService : DomainService
+    {
+        private readonly IRepository<Citizen, int> _citizenRepository;
+        private readonly UnitOfWorkManager _unitOfWorkManager;
+
+        public CitizenDomainService(IRepository<Citizen, int> citizenRepository, UnitOfWorkManager unitOfWorkManager)
+        {
+            _citizenRepository = citizenRepository;
+            _unitOfWorkManager = unitOfWorkManager;
+        }
+
+        [UnitOfWork]
+        public virtual async Task<Citizen> Create(
+            string name, 
+            string surname, 
+            decimal size, 
+            decimal weight, DateOnly birthDate,
+            string jobName, int phoneNumber, 
+            BloodType bloodType,
+            bool isDoingDrug = false, 
+            bool haveInsurance = false
+        )
+        {
+            var existingCitizen = await _citizenRepository.FirstOrDefaultAsync(c => c.Name == name && c.Surname == surname);
+            if(existingCitizen != null)
+            {
+                throw new BusinessException(EMSErrorCodes.CitizenAlreadyExist);
+            }
+
+            var newCitizen = new Citizen(name,surname,size,weight,birthDate,jobName,phoneNumber, bloodType, isDoingDrug, haveInsurance);
+            return await _citizenRepository.InsertAsync(newCitizen);
+        }
+
+        public virtual async Task<Citizen> Update(string name,
+            string surname,
+            decimal size,
+            decimal weight, DateOnly birthDate,
+            string jobName, int phoneNumber,
+            BloodType bloodType,
+            bool isDoingDrug = false,
+            bool haveInsurance = false)
+        {
+            var existingCitizen = await _citizenRepository.FirstOrDefaultAsync(c => c.Name == name && c.Surname == surname);
+            if (existingCitizen == null)
+            {
+                throw new BusinessException(EMSErrorCodes.CitizenDoNotExist);
+            }
+            existingCitizen = new Citizen(name, surname, size, weight, birthDate, jobName, phoneNumber, bloodType, isDoingDrug, haveInsurance);
+            return await _citizenRepository.UpdateAsync(existingCitizen);
+        }
+    }
+}
