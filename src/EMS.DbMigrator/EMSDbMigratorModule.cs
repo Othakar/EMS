d6466a742b781c11d629fc99fda50a13269a9327
thiba <thiba@DESKTOP-HD0VSKT>
2022-01-22 10:43:58 +0100

    Ajoutez des fichiers projet.

---
+++ src/EMS.DbMigrator/EMSDbMigratorModule.cs
@@ -0,0 +1,19 @@
+﻿using EMS.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Modularity;
+
+namespace EMS.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(EMSEntityFrameworkCoreModule),
+    typeof(EMSApplicationContractsModule)
+    )]
+public class EMSDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+    }
+}
---
+++ src/EMS.EntityFrameworkCore/EntityFrameworkCore/EMSEntityFrameworkCoreModule.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.AuditLogging.EntityFrameworkCore;
+using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.PostgreSql;
+using Volo.Abp.FeatureManagement.EntityFrameworkCore;
+using Volo.Abp.Identity.EntityFrameworkCore;
+using Volo.Abp.IdentityServer.EntityFrameworkCore;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement.EntityFrameworkCore;
+using Volo.Abp.TenantManagement.EntityFrameworkCore;
+
+namespace EMS.EntityFrameworkCore;
+
+[DependsOn(
+    typeof(EMSDomainModule),
+    typeof(AbpIdentityEntityFrameworkCoreModule),
+    typeof(AbpIdentityServerEntityFrameworkCoreModule),
+    typeof(AbpPermissionManagementEntityFrameworkCoreModule),
+    typeof(AbpSettingManagementEntityFrameworkCoreModule),
+    typeof(AbpEntityFrameworkCorePostgreSqlModule),
+    typeof(AbpBackgroundJobsEntityFrameworkCoreModule),
+    typeof(AbpAuditLoggingEntityFrameworkCoreModule),
+    typeof(AbpTenantManagementEntityFrameworkCoreModule),
+    typeof(AbpFeatureManagementEntityFrameworkCoreModule)
+    )]
+public class EMSEntityFrameworkCoreModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        EMSEfCoreEntityExtensionMappings.Configure();
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddAbpDbContext<EMSDbContext>(options =>
+        {
+                /* Remove "includeAllEntities: true" to create
+                 * default repositories only for aggregate roots */
+            options.AddDefaultRepositories(includeAllEntities: true);
+        });
+
+        Configure<AbpDbContextOptions>(options =>
+        {
+                /* The main point to change your DBMS.
+                 * See also EMSMigrationsDbContextFactory for EF Core tooling. */
+            options.UseNpgsql();
+        });
+    }
+}
