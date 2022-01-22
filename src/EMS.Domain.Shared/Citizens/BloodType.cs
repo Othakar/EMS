46caa04f8d6c67d6ced6f91a998dbe4993d9b4a5
Thibaut jallois <thibaut.jallois@outlook.fr>
2022-01-22 15:17:32 +0100

    Ajout enum groupe sanguin

---
+++ src/EMS.Domain.Shared/Citizens/BloodType.cs
@@ -0,0 +1,15 @@
+﻿namespace EMS.Citizens
+{
+    public enum BloodType
+    {
+        undefined = 0,
+        Aplus = 1,
+        Aminus = 2,
+        Bplus = 3,
+        Bminus = 4,
+        Oplus = 5,
+        Ominus = 6,
+        ABplus = 7,
+        ABminus = 8
+    }
+}
